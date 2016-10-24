8157a77b75541f04425ee81e2b7dfb6d37eeecb6
modeeb <modeeb@gmail.com>
2016-10-24 02:52:18 +0200

    Twin preview

---
+++ iot/iot-hub/AddTagsAndQuery/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace AddTagsAndQuery
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=qtmatters.azure-devices.net;DeviceId=inventory-mat;SharedAccessKeyName=device;SharedAccessKey=kD+P3NBu2tQ0Db9D/6vQX9Z3zgNSkHJGwiPL1i5w7LE=";
+
+        public static async Task AddTagsAndQuery()
+        {
+            var twin = await registryManager.GetTwinAsync("inventory-mat");
+            var patch =
+                @"{
+            tags: {
+                location: {
+                    region: 'US',
+                    plant: 'Redmond43'
+                }
+            }
+        }";
+            await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+
+            var query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43'", 100);
+            var twinsInRedmond43 = await query.GetNextAsTwinAsync();
+            Console.WriteLine("Devices in Redmond43: {0}", string.Join(", ", twinsInRedmond43.Select(t => t.DeviceId)));
+
+            query = registryManager.CreateQuery("SELECT * FROM devices WHERE tags.location.plant = 'Redmond43' AND properties.reported.connectivity.type = 'cellular'", 100);
+            var twinsInRedmond43UsingCellular = await query.GetNextAsTwinAsync();
+            Console.WriteLine("Devices in Redmond43 using cellular network: {0}", string.Join(", ", twinsInRedmond43UsingCellular.Select(t => t.DeviceId)));
+        }
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddTagsAndQuery().Wait();
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
